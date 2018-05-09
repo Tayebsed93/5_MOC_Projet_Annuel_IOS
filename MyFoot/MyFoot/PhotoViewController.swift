a1a745bd8895aedc0627c1fbf739167ca7574af9
Tayeb Sedraia <tayeb.sed@gmail.com>
2018-05-09 22:44:03 +0200

    ajout de la fonctionnalité photo snap (sans filtre)

---
+++ MyFoot/MyFoot/PhotoViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PhotoViewController.swift
+//  MyFoot
+//
+//  Created by Tayeb Sedraia on 09/05/2018.
+//  Copyright © 2018 Tayeb Sedraia. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    private var backgroundImage: UIImage
+    
+    init(image: UIImage) {
+        self.backgroundImage = image
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.black
+        let backgroundImageView = UIImageView(frame: view.frame)
+        backgroundImageView.contentMode = UIViewContentMode.scaleAspectFit
+        //backgroundImageView.contentMode = UIViewContentMode.scaleToFill
+        backgroundImageView.image = backgroundImage
+        view.addSubview(backgroundImageView)
+        let cancelButton = UIButton(frame: CGRect(x: 10.0, y: 10.0, width: 30.0, height: 30.0))
+        cancelButton.setImage(#imageLiteral(resourceName: "cancel"), for: UIControlState())
+        cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        view.addSubview(cancelButton)
+    }
+    
+    @objc func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+}
--- MyFoot/Pods/Target Support Files/Pods-MyFoot/Pods-MyFoot-frameworks.sh
+++ MyFoot/Pods/Target Support Files/Pods-MyFoot/Pods-MyFoot-frameworks.sh
@@ -146,11 +146,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DGElasticPullToRefresh/DGElasticPullToRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapSliderFilters/SnapSliderFilters.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyCam/SwiftyCam.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DGElasticPullToRefresh/DGElasticPullToRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapSliderFilters/SnapSliderFilters.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SwiftyCam/SwiftyCam.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
