bb716489a1871c728ff54923840c0fe8a8864df8
Tayeb Sedraia <tayeb.sed@gmail.com>
2018-05-11 18:34:59 +0200

    ajout de la fonctionnalité facebook

--- MyFoot/MyFoot/FacebookLoginViewController.swift
+++ MyFoot/MyFoot/FacebookLoginViewController.swift
@@ -7,21 +7,33 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
+import FBSDKCoreKit
 
 class FacebookLoginViewController: UIViewController {
 
     @IBOutlet weak var dismissButton: UIButton!
-    @IBOutlet weak var facebookBtn: UIButton!
-    
+    var dict : [String : AnyObject]!
     override func viewDidLoad() {
         super.viewDidLoad()
         dismissButton.layer.cornerRadius = dismissButton.frame.size.width / 2
         dismissButton.backgroundColor = .white
         dismissButton.setTitleColor(FACEBOOK_COLOR_BLUE, for: .normal)
+
+        //creating button
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
         
-        facebookBtn.layer.cornerRadius = dismissButton.frame.size.width / 2
-        facebookBtn.backgroundColor = .white
-        facebookBtn.setTitleColor(FACEBOOK_COLOR_BLUE, for: .normal)
+        //adding it to view
+        view.addSubview(loginButton)
+        
+        //if the user is already logged in
+        if let accessToken = AccessToken.current{
+            getFBUserData()
+            print("User is logged in with acess token: \(accessToken)")
+        }
+        
         view.backgroundColor = FACEBOOK_COLOR_BLUE
         // Do any additional setup after loading the view.
     }
@@ -36,8 +48,37 @@ class FacebookLoginViewController: UIViewController {
     }
     
     
-    @IBAction func facebookAction(_ sender: Any) {
+    //when login button clicked
+    @objc func loginButtonClicked() {
+        let loginManager = LoginManager()
+        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                self.getFBUserData()
+            }
+        }
     }
+    
+    //function is fetching the user data
+    func getFBUserData(){
+        if((AccessToken.current) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(self.dict)
+                }
+            })
+        }
+        
+
+    }
+    
+    
+
     
     
     /*
